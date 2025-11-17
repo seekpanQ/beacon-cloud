bbd6dd6a196ea0cb85e2f1941190f1c0616af33e
seekpan <lixin_qiu@163.com>
2025-11-17 21:52:06 +0800

    单条短信发送接口增加校验

--- beacon-api/src/main/java/com/mashibing/api/controller/SmsController.java
+++ beacon-api/src/main/java/com/mashibing/api/controller/SmsController.java
@@ -3,6 +3,10 @@ package com.mashibing.api.controller;
 import com.mashibing.api.form.SingleSendForm;
 import com.mashibing.api.utils.R;
 import com.mashibing.api.vo.ResultVO;
+import com.mashibing.common.enums.ExceptionEnums;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,10 +14,17 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/sms")
+@Slf4j
 public class SmsController {
 
     @RequestMapping(value = "single_send", produces = "application/json;charset=utf-8")
-    public ResultVO singleSend(@RequestBody SingleSendForm singleSendForm) {
+    public ResultVO singleSend(@RequestBody @Validated SingleSendForm singleSendForm, BindingResult bindingResult) {
+        //1. 校验参数
+        if (bindingResult.hasErrors()) {
+            String msg = bindingResult.getFieldError().getDefaultMessage();
+            log.info("【接口模块-单条短信Controller】 参数不合法 msg = {}", msg);
+            return R.error(ExceptionEnums.PARAMETER_ERROR.getCode(), msg);
+        }
         return R.ok();
     }
 
--- beacon-api/src/main/java/com/mashibing/api/form/SingleSendForm.java
+++ beacon-api/src/main/java/com/mashibing/api/form/SingleSendForm.java
@@ -1,6 +1,10 @@
 package com.mashibing.api.form;
 
 import lombok.Data;
+import org.hibernate.validator.constraints.Range;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 /**
  * 请求对象
@@ -10,16 +14,19 @@ public class SingleSendForm {
     /**
      * 客户的apikey
      */
+    @NotBlank(message = "apikey不允许为空")
     private String apikey;
 
     /**
      * 手机号
      */
+    @NotBlank(message = "手机号不能为空！")
     private String mobile;
 
     /**
      * 短信内容
      */
+    @NotBlank(message = "短信内容不能为空！")
     private String text;
 
     /**
@@ -30,5 +37,7 @@ public class SingleSendForm {
     /**
      * 0-验证码短信 1-通知类短信 2-营销类短信
      */
+    @Range(min = 0, max = 2, message = "短信类型只能是0~2的整数！")
+    @NotNull(message = "短信类型不允许非空！")
     private int state;
 }
