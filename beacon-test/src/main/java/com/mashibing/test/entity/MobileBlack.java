db45426dec6ff4301791ac33d7a14863397853d4
seekpan <lixin_qiu@163.com>
2025-12-05 21:30:21 +0800

    同步黑名单的数据到redis

---
+++ beacon-test/src/main/java/com/mashibing/test/entity/MobileBlack.java
@@ -0,0 +1,29 @@
+package com.mashibing.test.entity;
+
+/**
+ * @author zjw
+ * @description
+ */
+public class MobileBlack {
+
+    private String blackNumber;
+
+    private Integer clientId;
+
+
+    public String getBlackNumber() {
+        return blackNumber;
+    }
+
+    public void setBlackNumber(String blackNumber) {
+        this.blackNumber = blackNumber;
+    }
+
+    public Integer getClientId() {
+        return clientId;
+    }
+
+    public void setClientId(Integer clientId) {
+        this.clientId = clientId;
+    }
+}
