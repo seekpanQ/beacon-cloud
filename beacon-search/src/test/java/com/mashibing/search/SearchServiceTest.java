20e797c740d5de9bb81a13761b2e425a7d93b721
seekpan <lixin_qiu@163.com>
2025-12-12 00:01:53 +0800

    搭建搜索模块

---
+++ beacon-search/src/test/java/com/mashibing/search/SearchServiceTest.java
@@ -0,0 +1,13 @@
+package com.mashibing.search;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SearchServiceTest {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
