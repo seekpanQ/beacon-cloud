812c7caccbeb17b41034df9835ed1c25f0096b5e
seekpan <lixin_qiu@163.com>
2025-12-19 22:48:27 +0800

    短信网关module

---
+++ beacon-smsgateway/src/main/java/com/mashibing/smsgateway/SmsGatewayStarterApp.java
@@ -0,0 +1,16 @@
+package com.mashibing.smsgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class SmsGatewayStarterApp {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(SmsGatewayStarterApp.class, args);
+    }
+
+}
