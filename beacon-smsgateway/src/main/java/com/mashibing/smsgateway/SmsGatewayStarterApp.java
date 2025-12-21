30a047ba8a739498eeca99f1b5c3cd4daaeaa1fa
seekpan <lixin_qiu@163.com>
2025-12-21 10:35:10 +0800

    定义动态线程池

--- beacon-smsgateway/src/main/java/com/mashibing/smsgateway/SmsGatewayStarterApp.java
+++ beacon-smsgateway/src/main/java/com/mashibing/smsgateway/SmsGatewayStarterApp.java
@@ -1,11 +1,13 @@
 package com.mashibing.smsgateway;
 
+import cn.hippo4j.core.enable.EnableDynamicThreadPool;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableDynamicThreadPool
 public class SmsGatewayStarterApp {
 
     public static void main(String[] args) {
---
+++ beacon-smsgateway/src/main/java/com/mashibing/smsgateway/config/ThreadPoolConfig.java
@@ -0,0 +1,42 @@
+package com.mashibing.smsgateway.config;
+
+import cn.hippo4j.core.executor.DynamicThreadPool;
+import cn.hippo4j.core.executor.support.ThreadPoolBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.concurrent.ThreadPoolExecutor;
+
+/**
+ * 动态线程池的配置
+ */
+@Configuration
+public class ThreadPoolConfig {
+
+    @Bean
+    @DynamicThreadPool
+    public ThreadPoolExecutor cmppSubmitPool() {
+        String threadPoolId = "cmpp-submit";
+        ThreadPoolExecutor messageConsumeDynamicExecutor = ThreadPoolBuilder.builder()
+                // 指定线程名称的前缀
+                .threadFactory(threadPoolId)
+                // 线程池在Hippo4j中的唯一标识
+                .threadPoolId(threadPoolId)
+                // 代表动态线程池
+                .dynamicPool()
+                .build();
+        return messageConsumeDynamicExecutor;
+    }
+
+    @Bean
+    @DynamicThreadPool
+    public ThreadPoolExecutor cmppDeliverPool() {
+        String threadPoolId = "cmpp-deliver";
+        ThreadPoolExecutor messageProduceDynamicExecutor = ThreadPoolBuilder.builder()
+                .threadFactory(threadPoolId)
+                .threadPoolId(threadPoolId)
+                .dynamicPool()
+                .build();
+        return messageProduceDynamicExecutor;
+    }
+}
